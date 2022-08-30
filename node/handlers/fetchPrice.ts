b1a217b82147180b0b1c1ffc2bf1620095d3af31
Mateus Augusto Saggin <saggin.1994@gmail.com>
2022-08-30 02:42:18 -0300

    Fetch price with price table

--- node/handlers/fetchPrice.ts
+++ node/handlers/fetchPrice.ts
@@ -1,20 +1,52 @@
 import { NotFoundError } from '@vtex/api'
 
-export async function fetchPrice(
-  ctx: Context,
-  next: Next
-) {
+import type { Profile } from '../typings/profileSystem'
+
+export async function fetchPrice(ctx: Context, next: Next) {
   const { clients, body } = ctx
 
-  const { externalPrice } = clients
+  const { profileSystem, pricing } = clients
 
-  const price = await externalPrice.getPrice(body.item)
+  let currentProfile: Profile | null = null
 
-  if (!price) throw new NotFoundError('Price not found')
+  if (body.context.email) {
+    try {
+      currentProfile = (await profileSystem.getProfileInfo(
+        {
+          email: body.context.email,
+          userId: body.context.email,
+        },
+        'priceTables'
+      )) as Profile
+    } catch (e) {
+      ctx.vtex.logger.warn({
+        message: 'ExternalPriceApp_FetchPrice_NoProfile',
+        e,
+      })
+
+      throw e
+    }
+  }
+
+  const price = await pricing.getPrice(
+    body.item.skuId,
+    currentProfile?.priceTables ?? '1'
+  )
+
+  if (!price) {
+    const error = new NotFoundError('Price not found')
+
+    ctx.vtex.logger.error({
+      message: 'ExternalPriceApp_FetchPrice_Noprice',
+      error,
+    })
+
+    throw error
+  }
 
   ctx.state.quote = {
+    ...price,
     skuId: body.item.skuId,
-    price,
   }
 
   await next()
--- node/handlers/formatResponse.ts
+++ node/handlers/formatResponse.ts
@@ -2,18 +2,21 @@ import type { ExternalPriceResponse, Quote } from '../typings/externalPrice'
 
 export async function formatResponse(ctx: Context) {
   const { state, body } = ctx
-
   const quote = state.quote as Quote
 
+  // Checkout service expect prices coming from Pricing Service to be multiplied by 100
   const response: ExternalPriceResponse = {
     message: 'Price quoted successfully.',
     item: {
-      costPrice: null,
-      listPrice: null,
-      priceTables: '',
-      priceValidUntil: null,
+      price: quote.sellingPrice * 100,
+      priceTables: quote.tradePolicyId ?? '',
       index: body.item.index,
-      ...quote,
+      skuId: quote.skuId,
+      listPrice: quote.listPrice * 100,
+      costPrice: quote.costPrice * 100,
+      sellingPrice: quote.sellingPrice * 100,
+      priceValidUntil: quote.priceValidUntil,
+      tradePolicyId: quote.tradePolicyId,
     },
   }
 
